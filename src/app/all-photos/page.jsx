0ef39fea658eff5673812cf992ef6858eaf568cd
abubakkarsiddik123 <abu089604@gmail.com>
2026-06-22 01:15:46 +0600

    added all photos page

---
+++ src/app/all-photos/page.jsx
@@ -0,0 +1,19 @@
+import PhotoCard from '@/components/PhotoCard';
+import React from 'react';
+
+const AllPhotosPage = async() => {
+     const res = await fetch("http://localhost:3000//data.json")
+    const photos = await res.json();
+    return (
+        <div>
+            <h2 className='text-2xl font-bold my-5'>All Photos</h2>
+             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                {
+                    photos.map(photo=> <PhotoCard key={photo.id} photo={photo}/>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default AllPhotosPage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import Link from "next/link";
 
-
 const Navbar = () => {
   return (
     <div className="border-b px-2">
